8cea70488dba99d5a78fbc55b49af5f93ae13070
lemotion <1069183066@qq.com>
2019-08-22 16:28:32 +0800

    2. 模板 语法 {{ age.name.0}} {% for item in list %} {{ forloop.counter }} {% empty %} {% endfor %} {% if age > 18 %} {% endif %}

--- btemplate/views.py
+++ btemplate/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+
+    content = {
+        "name":"老王",
+        "age":88,
+        "son":[
+            "小明",
+            "小王",
+            "大头儿子",
+        ],
+        "wife":{
+            "name":"小丽"
+        }
+    }
+
     #渲染模板
-    return render(request,'index.html')
+    return render(request,'index.html',context=content)
