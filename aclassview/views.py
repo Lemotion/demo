b9b36885bf92d0b9457765dbd0e1caac1976acba
lemotion <1069183066@qq.com>
2019-08-22 11:51:01 +0800

    1.类视图添加 装饰器 1.1 装饰器的函数 1.2 Loginview_decorater() no attribute 'method' 1.3 改装饰器 添加 self参数 1.4 将装饰器 添加到 urls.py my_decorater(func())

--- aclassview/views.py
+++ aclassview/views.py
@@ -1,20 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+from django.views import View
 # Create your views here.
 
-#定义类视图
-from django.views import View
-
-
+def my_decorater(func):
+    def wrapper(self,request,*args,**kwargs):
+        print('添加了装饰器----函数')
+        print(request.method,request.path)
+        return func(self,request,*args,**kwargs)
+    return wrapper
+#定义类视图 添加装饰器
 class LoginView(View):
+    @my_decorater
     def get(self,request):
         # 处理GET请求，返回注册页面
-        return HttpResponse('视图函数-----GET------登录页面')
+        return HttpResponse('视图函数-----装饰器-----GET------登录页面')
 
     def post(self,reqeust):
         # 处理POST请求，实现注册逻辑
-        return HttpResponse('视图函数-----POST------登录请求')
+        return HttpResponse('视图函数-----装饰器-----POST------登录请求')
 
 def index(request):
     # 获取请求方法，判断是GET/POST请求
