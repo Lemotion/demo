536b664f99f314b280653ec8fc3804fb13e978f1
lemotion <1069183066@qq.com>
2019-08-22 14:57:51 +0800

    1.类视图添加 装饰器 添加到 classView 上面 @method_decorator(my_decorator,name="dispatch") @method_decorator(my_decorator,name="get") 添加到函数上面 @method_decorator(my_decorator) def get()

--- aclassview/views.py
+++ aclassview/views.py
@@ -1,23 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.utils.decorators import method_decorator
 
 from django.views import View
 # Create your views here.
 
-def my_decorater(func):
-    def wrapper(self,request,*args,**kwargs):
+def my_decorator(func):
+    def wrapper(request,*args,**kwargs):
         print('添加了装饰器----函数')
         print(request.method,request.path)
-        return func(self,request,*args,**kwargs)
+        return func(request,*args,**kwargs)
     return wrapper
+
+@method_decorator(my_decorator,name='dispatch')
 #定义类视图 添加装饰器
 class LoginView(View):
-    @my_decorater
+    # @method_decorator(my_decorator)
     def get(self,request):
         # 处理GET请求，返回注册页面
         return HttpResponse('视图函数-----装饰器-----GET------登录页面')
 
+    # @method_decorator(my_decorator)
     def post(self,reqeust):
         # 处理POST请求，实现注册逻辑
         return HttpResponse('视图函数-----装饰器-----POST------登录请求')
